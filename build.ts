442325ce72103af832241ed57b6da850ff226d64
Jesse Luoto <jesse.luoto@gmail.com>
2022-10-12 22:47:37 +0300

    Add Lighthouse perf testing
    Site already needed quite a lot of fixing:
    * Custom headers
    * Valid HTML and robots.txt

--- build.ts
+++ build.ts
@@ -2,6 +2,21 @@ import fs from "fs/promises";
 
 const build = async () => {
   await fs.mkdir("build", { recursive: true });
-  await fs.writeFile("build/index.html", "Hello world!");
+  await fs.writeFile(
+    "build/index.html",
+    `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <title>Sample web page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="This is a sample web page" />
+    </head>
+    <body>
+      <p>Hello world!</p>
+    </body>
+  </html>`
+  );
+  await fs.writeFile("build/robots.txt", `User-agent: *\nallow: /`);
 };
 build();
