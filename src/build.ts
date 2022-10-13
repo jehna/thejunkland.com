b0d088b470c22fe6b6aa33329e7008de8f68c6f3
Jesse Luoto <jesse.luoto@gmail.com>
2022-10-13 23:04:41 +0300

    Add React

--- src/build.ts
+++ src/build.ts
@@ -1,22 +1,13 @@
 import fs from 'fs/promises'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { Index } from './index'
 
 const build = async () => {
   await fs.mkdir('build', { recursive: true })
-  await fs.writeFile(
-    'build/index.html',
-    `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <title>Sample web page</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="This is a sample web page" />
-    </head>
-    <body>
-      <p>Hello world!</p>
-    </body>
-  </html>`
-  )
+
+  const content = renderToStaticMarkup(Index())
+
+  await fs.writeFile('build/index.html', `<!DOCTYPE html>${content}`)
   await fs.writeFile('build/robots.txt', 'User-agent: *\nallow: /')
 }
 build()
