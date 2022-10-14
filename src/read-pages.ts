c2217d0be9e2818348f35713d7c9f59f58d7f748
Jesse Luoto <jesse.luoto@gmail.com>
2022-10-14 22:16:00 +0300

    Add code highlighting

--- src/read-pages.ts
+++ src/read-pages.ts
@@ -4,6 +4,7 @@ import fs from 'fs'
 import { Page } from './templates/current-page-context'
 import { marked } from 'marked'
 import sizeOf from 'image-size'
+import highlight from 'highlight.js'
 
 const renderer = new marked.Renderer()
 renderer.image = (href, title, text) => {
@@ -19,7 +20,11 @@ export const readPages = (): Page[] => {
       contentKeyName: 'markdown'
     })
     return {
-      content: marked(markdown, { gfm: true, renderer }),
+      content: marked(markdown, {
+        gfm: true,
+        renderer,
+        highlight: highlightCustom
+      }),
       isBlogPost: file.startsWith('blog/'),
       isDraft: page.isDraft ?? false,
       menuOrder: page.menuOrder ?? 0,
@@ -38,4 +43,21 @@ export const readPages = (): Page[] => {
 const loadImageSize = (path: string) => {
   const size = sizeOf(path)
   return { width: size.width, height: size.height }
+}
+
+const highlightCustom = (code: string, lang: string) => {
+  if (lang === '' || lang === 'nohighlight') {
+    return code
+  }
+  if (lang === 'bash') {
+    return code
+      .split('\n')
+      .map(function (c) {
+        return '<span class="code-line">' + c + '</span>'
+      })
+      .join('\n')
+  }
+  return lang
+    ? highlight.highlight(code, { language: lang }).value
+    : highlight.highlightAuto(code).value
 }
